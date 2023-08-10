947c16780ba8f0d8f02e8a41bb3748b0702cb267
TORIFUKU Kaiou <torifuku.kaiou@gmail.com>
2023-08-10 23:12:36 +0900

    modified config/test.exs

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :hello, Hello.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOST") || "localhost",
   database: "hello_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
